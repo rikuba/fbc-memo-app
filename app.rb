0924e021d95832cee06eb6cdf9156d34e58ba451
Rikuba <rikuba@live.jp>
2021-02-02 10:45:09 +0900

    Implement memo update

--- app.rb
+++ app.rb
@@ -30,6 +30,13 @@ get '/memos/:memo_id' do |memo_id|
   erb :'memos/view', locals: { memo: memo }
 end
 
+patch '/memos/:memo_id' do |memo_id|
+  memo_store[memo_id] || halt(404)
+  title, content = params.values_at('title', 'content')
+  memo_store[memo_id] = { title: title, content: content }
+  redirect to("/memos/#{memo_id}"), 303
+end
+
 get '/memos/:memo_id/edit' do |memo_id|
   memo = memo_store[memo_id] || halt(404)
   erb :'memos/edit', locals: { memo: memo }
